43f41cb9a2a5e764da31f787662ddc1ec6cafaa4
Elimane Fofana <elimane.fofana@supinfo.com>
2022-08-17 19:50:31 +0200

    ok

--- src/main/java/com/springcloud/conversion/utils/CurrencyExchangeProxy.java
+++ src/main/java/com/springcloud/conversion/utils/CurrencyExchangeProxy.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @PROJECT conversion-service
  * @Author Elimane on 14/07/2022
  */
-@FeignClient(name = "currency-exchange", url = "localhost:8002")
+//@FeignClient(name = "currency-exchange", url = "localhost:8002")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
